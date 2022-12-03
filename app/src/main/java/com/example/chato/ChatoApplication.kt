f67b1bd3ea0f20f9660278776662fa89b6d883e9
Eslam Hamouda Ali <69871727+EslamHamouda@users.noreply.github.com>
2022-12-03 18:45:53 +0200

    initial commit

---
+++ app/src/main/java/com/example/chato/ChatoApplication.kt
@@ -0,0 +1,8 @@
+package com.example.chato
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ChatoApplication: Application() {
+}
---
+++ app/src/main/java/com/example/chato/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chato.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chato.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
